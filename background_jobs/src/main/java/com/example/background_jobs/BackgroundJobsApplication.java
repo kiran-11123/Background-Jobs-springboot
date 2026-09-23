4aeccadc2f08edab5ae35cb0eb06a5cad73d74f1
Kiran <kiran.sadara6730@gmail.com>
2026-09-23 19:39:32 +0530

    first commit

---
+++ background_jobs/src/main/java/com/example/background_jobs/BackgroundJobsApplication.java
@@ -0,0 +1,13 @@
+package com.example.background_jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackgroundJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackgroundJobsApplication.class, args);
+	}
+
+}
---
+++ background_jobs/src/test/java/com/example/background_jobs/BackgroundJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.background_jobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackgroundJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
